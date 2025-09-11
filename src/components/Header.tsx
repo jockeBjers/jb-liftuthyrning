82f1562bd71eb67def083e8457df5a4c7b12255b
JockeBjers <joakim.bjerselius@hotmail.com>
2025-09-11 11:25:27 +0200

    Add responsive hamburger menu for header. fix readme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
 import { Link, NavLink } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import routes from "../routes";
 
 export default function Header() {
-    return <header><Link to="/"><h1>Appstopher</h1></Link>
-        <nav>
-            {routes.filter(x => x.menuLabel).map(({ menuLabel, path }, i) =>
-                <NavLink key={i} to={path}>{menuLabel}</NavLink>)}
-        </nav>
-    </header>;
+    return (
+        <header className="bg-secondary">
+            <Navbar expand="lg" className="navbar p-0">
+                <Container fluid className="container px-0">
+                    <Navbar.Brand as={Link} to="/" className="ms-2">
+                        <h1 className=" text-black">Appstopher</h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle className="me-2" />
+                    <Navbar.Collapse className="navbar-collapse">
+                        <Nav className="navbar-nav ms-auto">
+                            {routes.filter(x => x.menuLabel).map(({ menuLabel, path }, i) =>
+                                <Nav.Link
+                                    key={i}
+                                    as={NavLink}
+                                    to={path}
+                                    className="nav-link px-lg-5" 
+                                >
+                                    {menuLabel}
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 }
