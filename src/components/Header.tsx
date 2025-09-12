77307bba897d97be55b8827d8bff9089837c7985
JockeBjers <joakim.bjerselius@hotmail.com>
2025-09-12 12:58:47 +0200

    fix Header component, add close mechanic to dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import routes from "../routes";
+import { useState } from "react";
 
 export default function Header() {
+const [expanded, setExpanded] = useState(false);
+
     return (
-        <header className="bg-secondary">
-            <Navbar expand="lg" className="navbar p-0">
+         <header className="bg-secondary">
+            <Navbar expanded={expanded} expand="lg" className="navbar p-0">
                 <Container fluid className="container px-0">
-                    <Navbar.Brand as={Link} to="/" className="ms-2">
-                        <h1 className=" text-black">Appstopher</h1>
+                    <Navbar.Brand as={Link} to="/" className="ms-2 text-decoration-none">
+                        <h1 className="fs-1 ">JB-Liftuthyrning</h1>
                     </Navbar.Brand>
-                    <Navbar.Toggle className="me-2" />
+                    <Navbar.Toggle className="bg-primary border-0 m-2 rounded-0 p-1" onClick={() => setExpanded(!expanded)} />
                     <Navbar.Collapse className="navbar-collapse">
                         <Nav className="navbar-nav ms-auto">
                             {routes.filter(x => x.menuLabel).map(({ menuLabel, path }, i) =>
@@ -18,7 +21,8 @@ export default function Header() {
                                     key={i}
                                     as={NavLink}
                                     to={path}
-                                    className="nav-link px-lg-5" 
+                                    className="nav-link px-lg-5"
+                                    onClick={() => setTimeout(() => setExpanded(false), 200)}
                                 >
                                     {menuLabel}
                                 </Nav.Link>
