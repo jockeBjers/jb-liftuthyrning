85ccbf784d02b1410a49b9d861be9e788d921e81
JockeBjers <joakim.bjerselius@hotmail.com>
2025-09-17 13:10:07 +0200

    add Admin page with route and loaders

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,9 @@ import ProductPage from "./pages/products/ProductPage";
 import ProductDetailsPage from "./pages/products/productDetailsPage/ProductDetailsPage";
 import LoginPage from "./pages/authentication/LoginPage";
 import RegisterPage from "./pages/authentication/RegisterPage";
+import UserPage from "./pages/user/UserPage";
+import AdminPage from "./pages/admin/AdminPage";
+
 
 interface Route {
     element: JSX.Element;
@@ -18,6 +21,21 @@ const routes: Route[] = [
     { element: <ProductPage />, path: '/Products', menuLabel: 'Produkter', loader: async () => { const response = await fetch('/api/lifts'); return response.json(); } },
     { element: <AboutPage />, path: '/AboutUs', menuLabel: 'Om oss' },
     { element: <LoginPage />, path: '/Login', menuLabel: 'Logga in' },
+    { element: <UserPage />, path: '/profile', menuLabel: 'Min sida' },
+    {
+        element: <AdminPage />, path: '/admin', menuLabel: 'Hemlig admin',
+        loader: async () => {
+            const lifts = await (await fetch('/api/lifts')).json();
+            const fuels = await (await fetch('/api/fuels')).json();
+            const lift_categories = await (await fetch('/api/lift_categories')).json();
+            const orders = await (await fetch('/api/orders')).json();
+            const orderItems = await (await fetch('/api/order_items')).json();
+            const users = await (await fetch('/api/users')).json();
+            const customerWithOrders = await (await fetch('/api/customerWithOrders')).json();
+            return { lifts, fuels, lift_categories, orders, orderItems, users, customerWithOrders };
+        }
+    },
+
     {
         element: <ProductDetailsPage />, path: '/products/:id', loader: async ({ params }: { params: { id: string } }) => {
             const response = await fetch(`/api/lifts/${params.id}`); return response.json();
