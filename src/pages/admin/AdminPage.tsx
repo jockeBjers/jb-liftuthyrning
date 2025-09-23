4c68bc01653fe216c2186c9dd667138ea394f029
JockeBjers <joakim.bjerselius@hotmail.com>
2025-09-23 19:44:48 +0200

    Add Nav to admin header instead of tab system.

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -4,9 +4,12 @@ import { NavLink, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
 import UserInfoCard from "../../components/userInfoCard";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useState } from "react";
 
 
 export default function AdminPage() {
+
+    const [expanded, setExpanded] = useState(false);
     const { user } = useAuth();
     if (!user) {
         return <div>Loading...</div>;
@@ -15,34 +18,54 @@ export default function AdminPage() {
     return (
         <div className="text-white mx-xs-1 mx-md-0 mt-0">
 
-            <header className="bg-body sticky-top pe-5">
-                <Navbar expand="lg" className="navbar p-0">
+            <header className="bg-body sticky-top pe-lg-5 pe-0">
+
+                <Navbar expanded={expanded} expand="lg" className="navbar p-0">
                     <Container fluid className="p-2">
+
                         <UserInfoCard user={user} />
+
+                        <Navbar.Toggle
+                            className="bg-primary border-0 m-2 rounded-0 p-1"
+                            onClick={() => setExpanded(!expanded)}
+                        />
                         <Navbar.Collapse className="navbar-collapse">
                             <Nav className="navbar-nav ms-auto">
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/admin" end>Dashboard</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/admin" end onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Dashboard
+                                    </Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/admin/orders">Ordrar</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/admin/orders" onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Ordrar
+                                    </Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/admin/customers">Kunder</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/admin/customers" onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Kunder
+                                    </Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/admin/lifts">Liftar</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/admin/lifts" onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Liftar
+                                    </Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/admin/categories">Bränsle & Kategorier</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/admin/categories" onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Bränsle & Kategorier
+                                    </Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link as={NavLink} to="/">Hemsida</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/" onClick={() => setExpanded(false)} className="px-lg-2 px-xl-4">
+                                        Hemsida
+                                    </Nav.Link>
                                 </Nav.Item>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+
             </header>
 
             <div className="mt-3 m-5">
