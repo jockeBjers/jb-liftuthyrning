11f030fa7dd474a7250d492477d755f50af10862
JockeBjers <joakim.bjerselius@hotmail.com>
2025-09-29 09:15:22 +0200

    update routes and db

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import StartPage from "./pages/start/StartPage";
 import AboutPage from "./pages/about-us/aboutPage";
 import ProductPage from "./pages/products/ProductPage";
 import ProductDetailsPage from "./pages/products/productDetailsPage/ProductDetailsPage";
+import BookingPage from "./pages/products/BookingPage";
 import LoginPage from "./pages/authentication/LoginPage";
 import RegisterPage from "./pages/authentication/RegisterPage";
 import UserPage from "./pages/user/UserPage";
@@ -13,7 +14,7 @@ import OrderTab from "./pages/admin/orders/OrderTab";
 import UserTab from "./pages/admin/users/UserTab";
 import ProductTab from "./pages/admin/products/ProductTab";
 import CategoryTab from "./pages/admin/categories/CategoryTab";
-import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminDashboard from "./pages/admin/dashboard/AdminDashboard";
 
 interface Route {
     element: JSX.Element;
@@ -25,7 +26,22 @@ interface Route {
 
 const routes: Route[] = [
     { element: <StartPage />, path: '', menuLabel: 'Start' },
-    { element: <ProductPage />, path: '/Products', menuLabel: 'Produkter', loader: async () => { const response = await fetch('/api/lifts'); return response.json(); } },
+    {
+        element: <ProductPage />, path: '/Products', menuLabel: 'Produkter', loader: async () => {
+            const liftDetails = await (await fetch("/api/liftDetails")).json();
+            const fuels = await (await fetch("/api/fuels")).json();
+            const liftCategories = await (await fetch("/api/liftCategories")).json();
+            return { liftDetails, fuels, liftCategories };
+        }
+    },
+    {
+        element: <BookingPage />, path: '/booking', menuLabel: 'Boka',
+
+        loader: async () => {
+            const liftDetails = await (await fetch("/api/liftDetails")).json();
+            return { liftDetails };
+        }
+    },
     { element: <AboutPage />, path: '/AboutUs', menuLabel: 'Om oss' },
     { element: <LoginPage />, path: '/Login', menuLabel: 'Logga in' },
     {
@@ -44,7 +60,16 @@ const routes: Route[] = [
         element: <ProtectedRoute requiredRole="admin"><AdminPage /></ProtectedRoute>,
         path: "/admin",
         children: [
-            {path: "", element: <AdminDashboard />},
+            {
+                path: "", element: <AdminDashboard />,
+                loader: async () => {
+                    const lifts = await (await fetch("/api/lifts")).json();
+                    const users = await (await fetch("/api/users")).json();
+                    const orders = await (await fetch("/api/orders")).json();
+                    const orderItems = await (await fetch("/api/orderItems")).json();
+                    return { lifts, users, orders, orderItems };
+                }
+            },
             {
                 path: "orders",
                 element: <OrderTab />,
@@ -61,8 +86,7 @@ const routes: Route[] = [
                 element: <UserTab />,
                 loader: async () => {
                     const users = await (await fetch("/api/users")).json();
-                    const customerWithOrders = await (await fetch("/api/customerWithOrders")).json();
-                    return { users, customerWithOrders };
+                    return { users };
                 }
             },
             {
@@ -70,7 +94,9 @@ const routes: Route[] = [
                 element: <ProductTab />,
                 loader: async () => {
                     const liftDetails = await (await fetch("/api/liftDetails")).json();
-                    return { liftDetails };
+                    const fuels = await (await fetch("/api/fuels")).json();
+                    const liftCategories = await (await fetch("/api/liftCategories")).json();
+                    return { liftDetails, fuels, liftCategories };
                 }
             },
             {
@@ -86,7 +112,7 @@ const routes: Route[] = [
     },
     {
         element: <ProductDetailsPage />, path: '/products/:id', loader: async ({ params }: { params: { id: string } }) => {
-            const response = await fetch(`/api/lifts/${params.id}`); return response.json();
+            const response = await fetch(`/api/liftDetails/${params.id}`); return response.json();
         }
     },
     { element: <RegisterPage />, path: '/register' },
